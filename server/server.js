467218d7a6f6087bad4c2f2f4ae192f07e7ee9cb
Ellie-Aghajani <z.aqajani@gmail.com>
2024-09-10 22:51:02 -0700

    basic server.js

--- server/server.js
+++ server/server.js
@@ -2,3 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("backend is up and running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app is listenning on port ${PORT}`);
+});
